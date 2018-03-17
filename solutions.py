8a4b8c71bdbc2fc69e35c76204ac96a64108e28d
Administrator <nolansingroy@gmail.com>
2018-03-16 21:51:34 -0500

    docs:Adding solutions.py Ive completed question 1 in solutions.py

---
+++ solutions.py
@@ -0,0 +1,17 @@
+############Q1########
+
+
+def compare(set1,set2):
+  for i in set!:
+    if i in set2:
+       if set1[i] === set2[i]:
+        set.pop(i)
+      else: return Flase
+    else: return Flase
+   if len(set2) == 0:
+    return True
+  else: 
+   return False
+
+
+
