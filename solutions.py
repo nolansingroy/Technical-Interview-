1775ec6a093b8672decdd674b5a2e20935721aed
Administrator <nolansingroy@gmail.com>
2018-03-17 22:07:21 -0500

    docs: Added answer to Question 2

--- solutions.py
+++ solutions.py
@@ -13,5 +13,40 @@ def compare(set1,set2):
   else: 
    return False
 
+def question2(a):
+    # make sure a is a string
+    if type(a) != str:
+        return "Error: a not string!"
+    
+    # make sure a has at least 2 characters
+    if len(a) < 2:
+        return a
 
+    # check all possible center of palindrome
+    pal_left = 0
+    pal_right = 1
+    for i in xrange(len(a)-1):
+        # check palindrome centered at i
+        l,r = longest_palindrome(a, i, i)
+        if r-l-1 > pal_right - pal_left:
+            pal_right = r
+            pal_left = l+1
+            
+        # check palindrome centered between i and i+1
+        l,r = longest_palindrome(a, i, i+1)
+        if r-l-1 > pal_right - pal_left:
+            pal_right = r
+            pal_left = l+1
+    return a[pal_left:pal_right]
+
+def test2():
+    print "\nTesting 2"
+    print "Edge case (not string):", "Pass" if "Error: a not string!" == question2(123) else "Fail"
+    print "Edge case (empty string):", "Pass" if "" == question2("") else "Fail"
+    print "Case (a = \"a\"):", "Pass" if "a" == question2("a") else "Fail" 
+    print "Case (a = \"aa\"):", "Pass" if "aa" == question2("aa") else "Fail"
+    print "Case (a = \"ab\"):", "Pass" if "a" == question2("ab") else "Fail"
+    print "Case (a = \"aba\"):", "Pass" if "aba" == question2("aba") else "Fail" 
+    print "Case (a = \"aaaaaba\"):", "Pass" if "aaaaa" == question2("aaaaaba") else "Fail" 
+    print "Case (a = \"abcbaiojadoijaosdj\"):", "Pass" if "abcba" == question2("abcbaiojadoijaosdj") else "Fail" 
 
