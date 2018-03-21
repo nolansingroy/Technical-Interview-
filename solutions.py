9bfe88e7f543a29c36a30c32a4325c7dbf3bf107
Nolan Singroy <nolansingroy@gmail.com>
2018-03-20 21:49:58 -0500

    docs: Added answer to question 5

--- solutions.py
+++ solutions.py
@@ -117,3 +117,81 @@ def question3(G):
         else:
             output_graph[i[2]] = [(i[1], i[0])]
     return output_graph
+
+
+
+
+
+
+############# For Question5 #############
+class Node(object):
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+def get_length(ll):
+    # get the length of ll
+    # also checking whether the linked list is circular
+    # return -1 if the linked list is circular
+
+    # length == 1
+    if ll.next == None:
+        return 1
+    
+    length_ll = 0
+    current_node = ll
+    current_node2 = ll.next
+    while current_node != None and current_node != current_node2:
+        current_node = current_node.next
+        if current_node2 != None:
+            current_node2 = current_node2.next
+        if current_node2 != None:
+            current_node2 = current_node2.next
+        length_ll += 1
+
+    if current_node == None:
+        return length_ll
+    else:
+        return -1
+
+def question5(ll, m):
+    # make sure ll is a Node
+    if type(ll) != Node:
+        return "Error: ll not a Node!"
+
+    # make sure m is an integer
+    if type(m) != int:
+        return "Error: m not an integer!"
+    
+    # get the length of ll
+    length_ll = get_length(ll)
+
+    # make sure ll is not circular
+    if length_ll == -1:
+        return "Error: circular linked list!"
+        
+    # make sure m is less than or equal to the length of ll
+    if length_ll < m:
+        return "Error: m greater than the length of ll!"
+    
+    # traverse to the last mth element
+    current_node = ll
+    for i in xrange(length_ll - m):
+        current_node = current_node.next
+        
+    return current_node.data
+
+def test5():
+    n1, n2, n3, n4, n5 = Node(1), Node(2), Node(3), Node(4), Node(5)
+    n4.next = n5
+    n3.next = n4
+    n2.next = n3
+    n1.next = n2
+    
+    print "\nTesting 5"
+    print "Edge case (ll not Node):", "Pass" if "Error: ll not a Node!" == question5(123, 111) else "Fail"
+    print "Edge case (m > length of ll):", "Pass" if "Error: m greater than the length of ll!" == question5(n1, 6) else "Fail"
+    print "Case (ll = n1 and m = 3):", "Pass" if 3 == question5(n1, 3) else "Fail" 
+    n5.next = n1
+    print "Case (circular linked list):", "Pass" if "Error: circular linked list!" == question5(n1, 3) else "Fail" 
+
