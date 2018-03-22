fc077733c52272ca69c4c082cf99d80505eec272
duke <nolansingroy@gmail.com>
2018-03-21 21:20:43 -0500

    docs:Revised question 2 solution

--- solutions.py
+++ solutions.py
@@ -1,4 +1,4 @@
-############Q1########
+############   Question 1    ########
 
 def question1(s,t):
     import collections
@@ -26,59 +26,60 @@ print ("\n Q1 results :\n")
 print (question1("Udacity", "ud")) #True
 print (question1("udacity", "ciy")) #False
 print (question1("ty", "udacity")) # fFalse 
-# should print False
-print (question1("", "")) 
-# should print True 
+print (question1("", "")) # true 
 
-########################
-def compare(set1,set2):
-  for i in set!:
-    if i in set2:
-       if set1[i] === set2[i]:
-        set.pop(i)
-      else: return Flase
-    else: return Flase
-   if len(set2) == 0:
-    return True
-  else: 
-   return False
+
+##########  Question 2 ##############
+
+def longest_palindrome(a, left_index, right_index):
+    # Left_index and right_index are the left and the right element of index
+    l = left_index
+    r = right_index
+    while l >= 0 and r < len(a):
+        if a[l] == a[r]:
+            l -= 1
+            r += 1
+        else:
+            return l, r
+    return l, r
+
 
 def question2(a):
-    # make sure a is a string
-    if type(a) != str:
-        return "Error: a not string!"
-    
-    # make sure a has at least 2 characters
+    # Make sure a is a string
+    if not isinstance(a, str):
+        return "Error input is !string"
+
+    # Make sure a has at least 2 characters
     if len(a) < 2:
-        return a
+        return "sorry you need more then two letter for me to compute :/"
 
-    # check all possible center of palindrome
-    pal_left = 0
-    pal_right = 1
-    for i in xrange(len(a)-1):
-        # check palindrome centered at i
-        l,r = longest_palindrome(a, i, i)
-        if r-l-1 > pal_right - pal_left:
-            pal_right = r
-            pal_left = l+1
-            
-        # check palindrome centered between i and i+1
-        l,r = longest_palindrome(a, i, i+1)
-        if r-l-1 > pal_right - pal_left:
-            pal_right = r
-            pal_left = l+1
-    return a[pal_left:pal_right]
+    # Check all possible centers of palindrome
+    p_left = 0
+    p_right = 1
+    for i in xrange(len(a) - 1):
+        # Check palindrome is centered at i
+        l, r = longest_palindrome(a, i, i)
+        if r - l - 1 > p_right - p_left:
+            p_right = r
+            p_left = l + 1
 
-def test2():
-    print "\nTesting 2"
-    print "Edge case (not string):", "Pass" if "Error: a not string!" == question2(123) else "Fail"
-    print "Edge case (empty string):", "Pass" if "" == question2("") else "Fail"
-    print "Case (a = \"a\"):", "Pass" if "a" == question2("a") else "Fail" 
-    print "Case (a = \"aa\"):", "Pass" if "aa" == question2("aa") else "Fail"
-    print "Case (a = \"ab\"):", "Pass" if "a" == question2("ab") else "Fail"
-    print "Case (a = \"aba\"):", "Pass" if "aba" == question2("aba") else "Fail" 
-    print "Case (a = \"aaaaaba\"):", "Pass" if "aaaaa" == question2("aaaaaba") else "Fail" 
-    print "Case (a = \"abcbaiojadoijaosdj\"):", "Pass" if "abcba" == question2("abcbaiojadoijaosdj") else "Fail" 
+        # Check palindrome centered is between i and i+1
+        l, r = longest_palindrome(a, i, i + 1)
+        if r - l - 1 > p_right - p_left:
+            p_right = r
+            p_left = l + 1
+    return a[p_left:p_right]
+
+
+
+# Test Cases
+
+print question2("racecar") # racecar
+print question2(1) #  Error input is !string
+print question2("z") #  sorry you need more then two letters for me to compute
+
+
+
 
 
 ############# For Question3 #############
